02308567c2506c6d3cf3d028bf1978771575bdcc
truongvv <truongvv@gmail.com>
2023-09-29 16:03:15 +0700

    update settings

--- project/app/api/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.api_v1.endpoints import access, gender_detect, gender_detect_open
-
-api_router = APIRouter()
-api_router.include_router(access.router)
-api_router.include_router(gender_detect.router)
-api_router.include_router(gender_detect_open.router)
--- project/app/utils/common.py
+++
@@ -1,8 +0,0 @@
-import pendulum, datetime
-
-
-def get_current_vn_time(f_mat="%Y-%m-%d %H:%M:%S"):
-    local_tz = pendulum.timezone("Asia/Bangkok")
-
-    return datetime.now(local_tz).strftime(f_mat)
-
